079a7f8f91765d52774d28ee20be8a81bb703272
Vishesh <visheshsingh16@gmail.com>
2019-01-02 16:06:42 -0500

    Creating modal order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuidControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -72,6 +74,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <Fragment>
+                <Modal><OrderSummary ingredients={this.state.ingredients} /></Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls ingredientAdded={this.addIngredient} ingredientRemoved={this.removeIngredient}
                     disabled={disabledInfo}
