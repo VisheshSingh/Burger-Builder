35797404014a17ff1f6ead36a7e2a23ac82422e8
Vishesh <visheshsingh16@gmail.com>
2019-01-02 14:09:31 -0500

    Adding Ingredients

--- src/components/Burger/BuildControls/BuidControls.js
+++ src/components/Burger/BuildControls/BuidControls.js
@@ -13,7 +13,10 @@ const BuidControls = (props) => {
     return (
         <div className={classes.BuildControls}>
             {controls.map(control => {
-                return <BuildControl key={control.label} label={control.label} />
+                return <BuildControl
+                    key={control.label}
+                    label={control.label}
+                    added={() => props.ingredientAdded(control.type)} />
             })}
         </div>
     )
